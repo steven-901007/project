02b2c2bde27ed6994924c7c6f6df84e3d5ce8938
40957024O-steven <stevenwsp901007@gmail.com>
2023-07-07 21:17:15 +0800

    Create storm_tracker_st_no.py

---
+++ 大氣相關/台大大學部暑期研究計畫/storm_tracker_st_no.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:/Users/steve/Desktop/python相關資料/NTU探空/storm_tracker/20220527/2022052709.u.st/st/no_2880_20220527_1600.csv",encoding = 'BIG5')
+# print(df)
+# Time = []
+# Channel = []
+# PacketID = []
+# Temperature = []
+# Humidity = []
