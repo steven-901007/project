016ab3c3cfbc38495632fa25535146c9e1e4e6a0
40957024O-steven <stevenwsp901007@gmail.com>
2024-03-01 16:48:59 +0800

    Update CA1_繪圖.py

--- school/熱力學/CA1/CA1_繪圖.py
+++ school/熱力學/CA1/CA1_繪圖.py
@@ -8,11 +8,13 @@ wb = load_workbook("C:/Users/steve/python_data/thermodynamics/CA1/Temperature.xl
 ws = wb['Temperature']
 X = ws.max_column-1
 Y = ws.max_row-1
-print(X,Y)
+# print(X,Y)
 
 DATA = []
 P = []
 H = []
+TIME = []
+
 #有效資料認證
 for h in range(Y-1):
     data_list = []
@@ -22,13 +24,26 @@ for h in range(Y-1):
         data_list.append(data)
     if data_list.count(-999) == 0:
         DATA.append(data_list)
-        P.append(ws.cell(h+2,1).value)
-        H.append(ws.cell(h+2,2).value)
-# print(DATA) [T]
-# print(P) [pa]
-# print(H) [m]
+        P.append(ws.cell(h+2,1).value/100)
+        H.append(ws.cell(h+2,2).value/1000)
+for t in range(X-1):
+    TIME.append(ws.cell(1,t+3).value)
+# print(DATA) #[T] DATA[[same hight]]
+# print(P) #[hpa]
+# print(H) #[km]
+# print(TIME)
 
-        2021/03/01 資料處理完畢剩下繪圖
+
+data_ave = []
+for h in range(len(DATA)):
+    ave = 0
+    for t in range(len(DATA[h])):
+           ave += DATA[h][t]
+    data_ave.append(ave/len(DATA[h]))
+    
+# print(data_ave)    
+
+
 
 
 #繪圖區
@@ -39,32 +54,50 @@ plt.rcParams['axes.unicode_minus'] = False
 fig = plt.figure()
 ax = fig.add_subplot()
 
-hight = []
-for i in range(Y):
-    hight.append(ws.cell(i+2,1).value/1000)
-# print(hight)
+
+time_to_print = ['06:00','12:00','18:00']
             
-for i in range(X):
+for i in range(len(DATA[0])):
     data = []
-    for h in range(Y):
-        data.append(ws.cell(h+2,i+2).value)
+    if time_to_print.count(TIME[i]) != 0:
+        for h in range(len(DATA)):
+            data.append(DATA[h][i])
     # print(data)
-    ax.plot(data,hight)
+        ax.plot(data,H,lw = 2,label = TIME[i]) #繪製不同圖要改第二個變數
 
 
 
-data_ave = []
-for h in range(Y):
-    sum = 0
-    for t in range(X):
-        sum+= ws.cell(h+2,t+2).value
-    data_ave.append(sum/X)
-    
-ax.plot(data_ave,hight,color ='black',lw = 3,label = 'average')
+#等高座標繪圖
+H_tick_set = [H[0]]
+for i in range(round(max(H))-round(min(H))+1):
+    H_tick_set.append(i+1)
+# print(H_tick_set)
+ax.plot(data_ave,H,color ='black',lw = 1,label = 'average')
+ax.set_ylim(H[0])
+ax.set_yticks(H_tick_set)
+plt.ylabel('Height[km]',fontsize = 20)
+        
+
+#等壓座標作圖
+# P_tick_set = []
+# for i in range(round(min(P)),round(max(P))-round(min(P))+1,100):
+#     P_tick_set.append(i+1)
+# P_tick_set.append(max(P))
+# P_tick_set.reverse()
+# print(P_tick_set)
+# ax.plot(data_ave,P,color ='black',lw = 1,label = 'average')
+# ax.invert_yaxis()
+# ax.set_ylim(P[len(P)-1])
+# ax.set_yticks(P_tick_set)
+# plt.ylabel('Pressure[mb]',fontsize = 20)
+
+
+
 ax.tick_params(axis='x', labelsize=12) 
 ax.tick_params(axis='y', labelsize=12) 
+
 plt.legend()
 plt.xlabel('Temperature[°C]',fontsize = 20)
-plt.ylabel('Height[km]',fontsize = 20)
+
 plt.title('tpe20110802cln\n苗栗站(24.56457N,120.82458E)',fontsize = 20)
 plt.show()
