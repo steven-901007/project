bf89bd69d48431cac6e832404c79dfd9b38a763f
40957024O-steven <stevenwsp901007@gmail.com>
2024-06-11 23:04:00 +0800

    20240611

--- 研究所/地圖標記點(經緯度list).py
+++ 研究所/地圖標記點(經緯度list).py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-
+## 無縣市邊界
 
 
 # 設定標記點的經緯度
-point_lon = [122.5704,119]
+point_lon = [121,121.5]
 point_lat = [23.7466,22]
 
 # 設定經緯度範圍
-lon_min, lon_max = 118, 123
-lat_min, lat_max = 21, 26
+lon_min, lon_max = 120, 122.1
+lat_min, lat_max = 21.5, 25.5
 
 # 讀取背景圖片
 background_image_path = "C:/Users/steve/GitHub/project/研究所/台灣地圖背景.png"  # 替換為你的圖片路徑
@@ -29,7 +29,7 @@ ax.set_title('Coordinate Point with Background Image')
 ax.imshow(background_image, extent=[lon_min, lon_max, lat_min, lat_max], aspect='auto')
 
 # 標記經緯度點
-ax.scatter(point_lon, point_lat, color='red', s=100)  # s 參數控制點的大小
+ax.scatter(point_lon, point_lat, color='red', s=100,zorder=5)  # s 參數控制點的大小
 
 
 # 顯示地圖
--- 研究所/雨量資料/雨量站分佈圖.py
+++ 研究所/雨量資料/雨量站分佈圖.py
@@ -3,22 +3,43 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.io.shapereader import Reader
 from cartopy.feature import ShapelyFeature
+import re
 
-# 讀取經緯度資料
-data_path = "C:/Users/steve/python_data/研究所/雨量資料/rain_gauge_lat_lon.txt"
-lon_data_list = []  # 經度
-lat_data_list = []  # 緯度
+## 讀取雨量站經緯度資料
+def rain_station_location_data():
+    data_path = "C:/Users/steve/python_data/研究所/雨量資料/2021_06/06/20210601/202106010000.QPESUMS_GAUGE.10M.mdf"
+    lon_data_list = []  # 經度
+    lat_data_list = []  # 緯度
+    count_for_station_number = 0 #雨量站數量
+    tatal_count_for_station_number = 0 #實際雨量站數量(包含不列入研究考量)之雨量站
 
-with open(data_path, encoding='utf-8', errors='replace') as file:
-    lines = file.readlines()  # 讀取所有行
-    for line in lines:
-        data = line.strip().split()
-        lon_data_list.append(float(data[1]))
-        lat_data_list.append(float(data[2]))
+    line = 0
+    with open(data_path, 'r') as files:
+        for file in files:
+            if line >=3:
+                data = re.split(re.compile(r'\s+|\n|\*'),file.strip())
+                print(data)
+                if 120 <float(data[4])< 122.1 and 21.5 <float(data[3])< 25.5:
+                    lon_data_list.append(float(data[4]))
+                    lat_data_list.append(float(data[3]))
+                    count_for_station_number += 1
+                tatal_count_for_station_number += 1 
+            line +=1
+    print(tatal_count_for_station_number)
+    return lon_data_list, lat_data_list ,count_for_station_number ,tatal_count_for_station_number
 
-# 繪圖
+lon_data_list, lat_data_list ,count_for_station_number ,tatal_count_for_station_number = rain_station_location_data()
+
+## 繪圖
+
+# 設定經緯度範圍
+lon_min, lon_max = 120, 122.1
+lat_min, lat_max = 21.5, 25.5
+
 plt.figure(figsize=(10, 10))
 ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_xlim(lon_min, lon_max)
+ax.set_ylim(lat_min, lat_max)
 
 plt.rcParams['font.sans-serif'] = [u'MingLiu']  # 設定字體為'細明體'
 plt.rcParams['axes.unicode_minus'] = False  # 用來正常顯示正負號
@@ -41,7 +62,7 @@ ax.scatter(lon_data_list, lat_data_list, color='red', s=3, zorder=5)
 # 加入標籤
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-ax.set_title('全台雨量站座標')
+ax.set_title('全台雨量站座標\n雨量站數量：' + str(count_for_station_number))
 
 # 顯示地圖
 plt.show()
