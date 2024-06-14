bff9702501ddea11eee74dc718314ffc908c95d9
40957024O-steven <stevenwsp901007@gmail.com>
2024-06-14 22:11:34 +0800

    20240614

--- 研究所/雨量資料/對流性降雨_事件數.py
+++ 研究所/雨量資料/對流性降雨_事件數.py
@@ -1,7 +1,4 @@
 import glob
-import re
-import math
-import time
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -36,53 +33,40 @@ def rain_station_location_data():
 lon_data_list, lat_data_list ,name_data_list = rain_station_location_data()
 
     
-data_name_of_station_list = [] #地點
-data_rain_list = [] #降雨量
-data_time_list = [] #時間
-data_lon_list = []
-data_lat_list = []
+rain_data_name_of_station_nonstd_list = [] #地點
 
-## 讀取每月資料
-month_path = "C:/Users/steve/python_data/研究所/雨量資料/"+year+"_"+month+"/"+month
-result  =glob.glob(month_path+"/*")
-for day_path in result:
-    # print(day_path)
-    day = day_path[53:] #日期
-    print('日期:'+day)
-
-
-    ## 讀取每日資料
-    result  =glob.glob(day_path+'/*')
-    for rain_data_path in result:
-        # print(rain_data_path)
-        hour = rain_data_path[64:66]
-        minute  =rain_data_path[66:68]
-        # print(hour+":"+minute)
+## 讀取降雨資料
+rain_data_path = "C:/Users/steve/python_data/研究所/雨量資料/對流性降雨data/2021/2021_06.xlsx"
+wb_rain_data = load_workbook(rain_data_path)
+month_list = wb_rain_data.sheetnames
+# print(month_list)
+for month in month_list:
+    ws_rain_data = wb_rain_data[month]
+    print('月份：'+month)
+    max_col_rain_data = ws_rain_data.max_column
+    for col in range(1,max_col_rain_data+1):
+        row = 2
+        print(col)
+        while ws_rain_data.cell(row,col).value != None:
+            rain_data_name_of_station_nonstd_list.append(ws_rain_data.cell(row,col).value)
+            print(ws_rain_data.cell(row,col).value)            
+            row += 1
 
 
-        ## 每日資料處理 rain data >10mm (10min)
-        line = 0
-        with open(rain_data_path, 'r') as files:
-            for file in files:
-                elements = re.split(re.compile(r'\s+|\n|\*'),file.strip())
-                # print(elements)
-                # print(len(elements))
-                if line >= 3 :
-                    station_name = elements[0]
-                    rain_data_of_10min = float(elements[7]) #MIN_10
-                    if rain_data_of_10min >= 0: #排除無資料(data = -998.00)
-                        rain_data_of_3_hour = float(elements[8]) #HOUR_3
-                        rain_data_of_6_hour = float(elements[9]) #HOUR_6
-                        rain_data_of_12_hour = float(elements[10]) #HOUR_12
-                        rain_data_of_24_hour = float(elements[11]) #HOUR_24
-                        if 10<=rain_data_of_10min <= rain_data_of_3_hour <= rain_data_of_12_hour <= rain_data_of_24_hour: #QC
-                            data_rain_list.append(rain_data_of_10min)
-                            # data_name_of_station_list.append(station_name)
-                            data_lon_list.append(float(elements[4]))
-                            data_lat_list.append(float(elements[3]))
-                line += 1
 
-# print(data_lon_list,data_lat_list)
+#統計每個站點的降雨次數
+# rain_data_name_of_station_list = []
+rain_data_lon_list = []
+rain_data_lat_list = []
+rain_count = [] #降雨次數
+for data in rain_data_name_of_station_nonstd_list:
+    rain_count.append(rain_data_name_of_station_nonstd_list.count(data))
+    rain_data_lon_list.append(lon_data_list[name_data_list.index(data)])
+    rain_data_lat_list.append(lat_data_list[name_data_list.index(data)])
+
+# print(rain_count)
+print(sum(rain_count))
+
 
 
 
@@ -116,24 +100,23 @@ gridlines.right_labels = False
 color_list = []
 level = [0,3,5,10,15,20,30,35]
 color_box = ['none','black','grey','silver','whitesmoke','gold','lightcoral','orangered']
-print(len(data_lon_list))
 
-for i in range(len(data_lon_list)):
+
+for nb in rain_count:
     more_then_maxma_or_not = 0
-    lon_lat_count = min([data_lon_list.count(data_lon_list[i]),data_lat_list.count(data_lat_list[i])])#避免某個地方的經度or緯度一樣(but 經度and緯度不一樣)
     for j in range(len(level)-1):
-        if level[j]<lon_lat_count<=level[j+1]:
+        if level[j]<nb<=level[j+1]:
             color_list.append(color_box[j+1])
             more_then_maxma_or_not = 1
             break
     if more_then_maxma_or_not == 0:
         color_list.append('lime')
-        print(lon_lat_count)
+        print(nb)
 # print(len(color_list))
 
 
 # 標記經緯度點
-ax.scatter(data_lon_list, data_lat_list, color=color_list, s=3, zorder=5)
+ax.scatter(rain_data_lon_list, rain_data_lat_list, color=color_list, s=3, zorder=5)
 
 # colorbar setting
 
