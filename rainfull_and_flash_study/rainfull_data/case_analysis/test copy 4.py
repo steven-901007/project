4a9a5433f9335524638e28b91ad097019ed96531
steven <stevenwsp901007@gmail.com>
2024-08-10 17:01:12 +0800

    20240810

--- rainfull_and_flash_study/rainfull_data/case_analysis/test copy 4.py
+++ rainfull_and_flash_study/rainfull_data/case_analysis/test copy 4.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 
 
@@ -10,3 +10,10 @@ data_top_path = "C:/Users/steve/python_data"
 dis = 36
 
 station = '466920'
+
+
+tg_station_data_path = f"C:/Users/steve/python_data/研究所/雨量資料/{year}測站資料.csv"
+
+data_path = f"{data_top_path}/研究所/雨量資料/{str(dis)}km個案分析/{month}/{station}/{station}_rain_data.xlsx"
+datas = pd.read_excel(data_path)
+print(datas)
