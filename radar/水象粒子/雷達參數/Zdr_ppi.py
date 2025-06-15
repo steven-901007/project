ba5f8aa855a2c852586adf861b2343122da6fd1b
steven <stevenwsp901007@gmail.com>
2025-06-15 13:43:23 +0800

    202505151343

--- radar/水象粒子/雷達參數/Zdr_ppi.py
+++ radar/水象粒子/雷達參數/Zdr_ppi.py
@@ -3,37 +3,34 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from datetime import datetime
 
+# === 路徑與時間設定 ===
 data_top_path = "C:/Users/steve/python_data/radar"
-year = '2024'
-month = '05'
-day = '23'
-hh = '00'
-mm = '02'
-ss = '00'
-
-file_path = f"{data_top_path}/{year}{month}{day}_u.RCWF/{year}{month}{day}{hh}{mm}{ss}.VOL"
+year, month, day = '2024', '05', '23'
+hh, mm, ss = '00', '02', '00'
+file_path = f"{data_top_path}/PID/{year}{month}{day}{hh}{mm}{ss}.nc"
 shapefile_path = f"{data_top_path}/Taiwan_map_data/COUNTY_MOI_1090820.shp"
-
 plt.rcParams['font.sans-serif'] = [u'MingLiu']
 plt.rcParams['axes.unicode_minus'] = False
-time = file_path.split('/')[-1].split('.')[0]
-time_dt = datetime.strptime(time, "%Y%m%d%H%M%S").strftime("%Y/%m/%d %H:%M:%S")
 
+# === 讀資料與時間字串 ===
 radar = pyart.io.read(file_path)
+time_str = file_path.split('/')[-1].split('.')[0]
+time_dt = datetime.strptime(time_str, "%Y%m%d%H%M%S").strftime("%Y/%m/%d %H:%M:%S")
+
 sweep_num = 2
 
-
+# === 畫 RHOHV PPI ===
 display = pyart.graph.RadarMapDisplay(radar)
 fig = plt.figure(figsize=(10, 10))
 ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
-display.plot_ppi_map('cross_correlation_ratio',
+display.plot_ppi_map('differential_reflectivity',
                      sweep=sweep_num,
                      ax=ax,
-                     colorbar_label='相關係數 ($\\rho_{HV}$)',
-                     title=f'cross correlation ratio\n{time_dt}',
-                     vmin=0.7,
-                     vmax=1.0,
+                     colorbar_label='差異反射率 ($Z_{DR}$) \n (dB)',
+                     title=f'differential reflectivity\n{time_dt}',
+                     vmin=-1.0,
+                     vmax=4.0,
                      shapefile=shapefile_path,
                      shapefile_kwargs={"facecolor": 'none', 'edgecolor': 'green'},
                      embellish=False)
--- radar/水象粒子/雷達參數/rhohv_ppi.py
+++ radar/水象粒子/雷達參數/rhohv_ppi.py
@@ -2,30 +2,30 @@ import pyart
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from datetime import datetime
-
+import numpy as np
+# === 路徑與時間設定 ===
 data_top_path = "C:/Users/steve/python_data/radar"
-year = '2024'
-month = '05'
-day = '23'
-hh = '00'
-mm = '02'
-ss = '00'
-
-file_path = f"{data_top_path}/{year}{month}{day}_u.RCWF/{year}{month}{day}{hh}{mm}{ss}.VOL"
+year, month, day = '2024', '05', '23'
+hh, mm, ss = '00', '02', '00'
+file_path = f"{data_top_path}/PID/{year}{month}{day}{hh}{mm}{ss}.nc"
 shapefile_path = f"{data_top_path}/Taiwan_map_data/COUNTY_MOI_1090820.shp"
-
 plt.rcParams['font.sans-serif'] = [u'MingLiu']
 plt.rcParams['axes.unicode_minus'] = False
-time = file_path.split('/')[-1].split('.')[0]
-time_dt = datetime.strptime(time, "%Y%m%d%H%M%S").strftime("%Y/%m/%d %H:%M:%S")
 
+# === 讀資料與時間字串 ===
 radar = pyart.io.read(file_path)
+time_str = file_path.split('/')[-1].split('.')[0]
+time_dt = datetime.strptime(time_str, "%Y%m%d%H%M%S").strftime("%Y/%m/%d %H:%M:%S")
+radar.fields['cross_correlation_ratio']['data'] = np.ma.masked_equal(
+    radar.fields['cross_correlation_ratio']['data'], -999
+)
 sweep_num = 2
 
-
+# === 畫 RHOHV PPI ===
 display = pyart.graph.RadarMapDisplay(radar)
 fig = plt.figure(figsize=(10, 10))
 ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
 
 display.plot_ppi_map('cross_correlation_ratio',
                      sweep=sweep_num,
