e00ec04e44f7e32d03c33a40a4be885c1fdef525
steven <stevenwsp901007@gmail.com>
2026-06-04 09:35:53 +0800

    20260604

--- radar/PID/PID_count_3in1.py
+++ radar/PID/PID_count_3in1.py
@@ -10,8 +10,8 @@ import os
 year = sys.argv[1] if len(sys.argv) > 1 else '2021'
 month = sys.argv[2] if len(sys.argv) > 2 else '05'
 day = sys.argv[3] if len(sys.argv) > 3 else '24'
-hh = sys.argv[4] if len(sys.argv) > 4 else '04'
-mm = sys.argv[5] if len(sys.argv) > 5 else '50'
+hh = sys.argv[4] if len(sys.argv) > 4 else '05'
+mm = sys.argv[5] if len(sys.argv) > 5 else '32'
 
 station = sys.argv[6] if len(sys.argv) > 6 else 'RCWF'
 point_num = sys.argv[7] if len(sys.argv) > 7 else '1'  # 預設點選兩個點
